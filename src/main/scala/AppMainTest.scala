bd2bccf3a969eeba39a096d92d72afc90eec8767
wyan <wyan@whtb.tech>
2019-05-23 18:19:14 +0800

    demo

---
+++ src/main/scala/AppMainTest.scala
@@ -0,0 +1,84 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
+class AppMainTest {
+
+  def main(args: Array[String]): Unit = {
+
+    val textFieldUrl = "/Users/lionyan/Desktop/ScalaStudy/20181022.txt"
+
+//    PV
+
+//    UV
+
+    TopN
+  }
+
+}
+
+
+object PV extends AppMainTest {
+
+  println("hello word")
+
+  //创建conf
+  private val sparkConf: SparkConf = new SparkConf().setAppName("PV").setMaster("local[2]")
+
+  //创建context对象
+  private val sc: SparkContext = new SparkContext(sparkConf)
+  //设置输出的日志级别
+  sc.setLogLevel("WARN")
+  //读取日志
+  private val dataRDD: RDD[String] = sc.textFile("/Users/lionyan/Desktop/ScalaStudy/20181022.txt")
+  //方式一：计算有多少行及PV总量
+  private val finalResult: Long = dataRDD.count()
+  println("result："+ finalResult)
+
+  //方式二：
+//  private val pvOne: RDD[(String, Int)] = dataRDD.map(x => ("PV", 1))
+//  private val resultPV: RDD[(String, Int)] = pvOne.reduceByKey(_ + _)
+//  resultPV.foreach(x => println(x))
+//  sc.stop()
+}
+
+object UV extends AppMainTest {
+
+  private val sparkConf: SparkConf = new SparkConf().setAppName("UV").setMaster("local[2]")
+
+  private val sparkContext: SparkContext = new SparkContext(sparkConf)
+
+  sparkContext.setLogLevel("WARN")
+
+  private val dataRDD: RDD[String] = sparkContext.textFile("/Users/lionyan/Desktop/ScalaStudy/20190522.txt")
+
+  private val ips: RDD[String] = dataRDD.map(_.split("insert")(0))
+
+  private val ipNum:Long = ips.distinct().count()
+
+  println(ipNum)
+
+//  sparkContext.stop()
+}
+
+object TopN extends AppMainTest{
+  //创建sparkConf对象
+  private val sparkConf: SparkConf = new SparkConf().setAppName("PV").setMaster("local[2]")
+  //创建SparkContext对象
+  private val sc: SparkContext = new SparkContext(sparkConf)
+  //设置输出的日志级别
+  sc.setLogLevel("WARN")
+  //读取日志数据
+  private val dataRDD: RDD[String] = sc.textFile("/Users/lionyan/Desktop/ScalaStudy/20190522.txt")
+  //对每一行的日志信息进行切分并且过滤清洗掉不符合规则的数据
+  //通过对日志信息的分析，我们知道按照空格切分后，下标为10的是url，长度小于10的暂且认为是不符合规则的数据
+  private val urlAndOne: RDD[(String, Int)] = dataRDD.filter(_.split("insert").size>10).map(x=>(x.split(" ")(10),1))
+  //相同url进行累加
+  private val result: RDD[(String, Int)] = urlAndOne.reduceByKey(_+_)
+  //访问最多的url并进行倒叙排序
+  private val sortResult: RDD[(String, Int)] = result.sortBy(_._2,false)
+  //取前五位
+  private val finalResult: Array[(String, Int)] = sortResult.take(5)
+  //打印输出
+  finalResult.foreach(println)
+  sc.stop()
+}
