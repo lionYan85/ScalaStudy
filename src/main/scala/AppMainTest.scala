ad6a46e4d5dfa5ceecf14bc8233851c22bb2dbd5
lionYan85 <38717814+lionYan85@users.noreply.github.com>
2019-07-12 01:09:55 +0800

    streaming

--- src/main/scala/AppMainTest.scala
+++ src/main/scala/AppMainTest.scala
@@ -1,5 +1,10 @@
+import java.io.PrintWriter
+import java.net.ServerSocket
+
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.StreamingContext
+import org.scalatest.time.Seconds
 
 class AppMainTest {
 
@@ -7,11 +12,11 @@ class AppMainTest {
 
     val textFieldUrl = "/Users/lionyan/Desktop/ScalaStudy/20181022.txt"
 
-//    PV
+    //    PV
 
-//    UV
+    //    UV
 
-    TopN
+    //    TopN
   }
 
 }
@@ -32,13 +37,13 @@ object PV extends AppMainTest {
   private val dataRDD: RDD[String] = sc.textFile("/Users/lionyan/Desktop/ScalaStudy/20181022.txt")
   //方式一：计算有多少行及PV总量
   private val finalResult: Long = dataRDD.count()
-  println("result："+ finalResult)
+  println("result：" + finalResult)
 
   //方式二：
-//  private val pvOne: RDD[(String, Int)] = dataRDD.map(x => ("PV", 1))
-//  private val resultPV: RDD[(String, Int)] = pvOne.reduceByKey(_ + _)
-//  resultPV.foreach(x => println(x))
-//  sc.stop()
+  //  private val pvOne: RDD[(String, Int)] = dataRDD.map(x => ("PV", 1))
+  //  private val resultPV: RDD[(String, Int)] = pvOne.reduceByKey(_ + _)
+  //  resultPV.foreach(x => println(x))
+  //  sc.stop()
 }
 
 object UV extends AppMainTest {
@@ -53,14 +58,14 @@ object UV extends AppMainTest {
 
   private val ips: RDD[String] = dataRDD.map(_.split("insert")(0))
 
-  private val ipNum:Long = ips.distinct().count()
+  private val ipNum: Long = ips.distinct().count()
 
   println(ipNum)
 
-//  sparkContext.stop()
+  //  sparkContext.stop()
 }
 
-object TopN extends AppMainTest{
+object TopN extends AppMainTest {
   //创建sparkConf对象
   private val sparkConf: SparkConf = new SparkConf().setAppName("PV").setMaster("local[2]")
   //创建SparkContext对象
@@ -68,17 +73,70 @@ object TopN extends AppMainTest{
   //设置输出的日志级别
   sc.setLogLevel("WARN")
   //读取日志数据
-  private val dataRDD: RDD[String] = sc.textFile("/Users/lionyan/Desktop/ScalaStudy/20190522.txt")
+  private val dataRDD: RDD[String] = sc.textFile("C:/Users/Administrator/Desktop/ScalaStudy/20190522.txt")
   //对每一行的日志信息进行切分并且过滤清洗掉不符合规则的数据
   //通过对日志信息的分析，我们知道按照空格切分后，下标为10的是url，长度小于10的暂且认为是不符合规则的数据
-  private val urlAndOne: RDD[(String, Int)] = dataRDD.filter(_.split("insert").size>10).map(x=>(x.split(" ")(10),1))
+  private val urlAndOne: RDD[(String, Int)] = dataRDD.filter(_.split("insert").size > 10).map(x => (x.split(" ")(10), 1))
   //相同url进行累加
-  private val result: RDD[(String, Int)] = urlAndOne.reduceByKey(_+_)
+  private val result: RDD[(String, Int)] = urlAndOne.reduceByKey(_ + _)
   //访问最多的url并进行倒叙排序
-  private val sortResult: RDD[(String, Int)] = result.sortBy(_._2,false)
+  private val sortResult: RDD[(String, Int)] = result.sortBy(_._2, false)
   //取前五位
   private val finalResult: Array[(String, Int)] = sortResult.take(5)
   //打印输出
   finalResult.foreach(println)
   sc.stop()
+}
+
+object GenerateChar {
+
+  def generateContext(index: Int): String = {
+    import scala.collection.mutable.ListBuffer
+    val charList = ListBuffer[Char]()
+    for (i <- 65 to 90)
+      charList += i.toChar
+    val charArray = charList.toArray
+    charArray(index).toString
+
+  }
+
+  def index = {
+    import java.util.Random
+    val rdm = new Random()
+    rdm.nextInt(7)
+  }
+
+  def main(args: Array[String]) {
+    val listener = new ServerSocket(9998)
+    while (true) {
+      val socket = listener.accept()
+      new Thread() {
+        override def run() = {
+          println("socket from" + socket.getInetAddress)
+          val out = new PrintWriter(socket.getOutputStream, true)
+          while (true) {
+            Thread.sleep(500)
+            val context = generateContext(index)
+            print(context)
+            out.write(context)
+            out.flush()
+          }
+          socket.close()
+        }
+      }.start()
+    }
+  }
+}
+
+object ScoketStreaming {
+  def main(args: Array[String]){
+    val conf = new SparkConf().setMaster("local[2]").setAppName("socketStreaming")
+    val sc = new StreamingContext(conf,Seconds(1))
+    val lines = sc.socketTextStream("master", 9998)
+    val words = lines.flatMap(_.split(" "))
+    val wordCount = words.map(x => (x, 1)).reduceByKey(_ + _);
+    wordCount.print()
+    sc.start()
+    sc.awaitTermination()
+  }
 }
